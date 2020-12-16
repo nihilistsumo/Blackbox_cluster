d725ceb94588584964eaab731a48b4b119507f99
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-12-16 13:09:13 -0500

    debugging cuda

--- models/cluster.py
+++ models/cluster.py
@@ -9,7 +9,7 @@ def clustering(batch_pairscore_matrix, num_clusters):
     cluster_labels = []
     for i in range(batch_pairscore_matrix.shape[0]):
         clustering_algo = AgglomerativeClustering(n_clusters=int(num_clusters[i].item()), affinity='precomputed', linkage='average')
-        cluster_labels.append(clustering_algo.fit_predict(batch_pairscore_matrix[i].cpu()))
+        cluster_labels.append(clustering_algo.fit_predict(batch_pairscore_matrix[i]))
     cluster_labels = np.array(cluster_labels)
 
     num_batch = batch_pairscore_matrix.shape[0]
